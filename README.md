f20909e1f4fc2cf38f9e94091011341db4273e4d
tim <tim.erlandsson01@gmail.com>
2026-07-28 17:29:40 +0200

    Added variable json list payload to task creation, preparing a task now sends list of what variables are needed. wrote to readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-My journey as a junior programmer building a mobile app that aims to simplify the creation and execution of running tasks on a server
+My journey as a junior programmer building a mobile app that aims to simplify the creation and execution of running tasks on a server.
+
+Easy Automate Api aims to simplify task execution and creation sush as bash scipts and python code. It runs all over Fast Api for easy javasript intergration.
+My personal goal is to explore how an app is "supposed" to be built, learning react or another framework, learning how to create and use apis to communicate with servers. 
--- api.py
+++ api.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI, Request
-from pydantic import BaseModel
+from pydantic import BaseModel, JsonValue
 import os
-import test
+import json
 
 defs = []
 
 app = FastAPI()
 
+
+#define json payloads for post requests
 class DownloadRequest(BaseModel):
     path:str
     url:str
@@ -18,17 +20,28 @@ class create_vars(BaseModel):
     bash:bool
     python:bool
     code:str
+    vars:JsonValue
+
+class retrieve_vars(BaseModel):
+    name:str
 
 
-
+#load user created tasks into defs list by reading file names
 def load_defs():
     global defs
     defs = []
     for (root,dirs,files) in os.walk("./userdeffs"):
         for file in files:
-            defs.append(file.replace(".txt", ""))
-            
-        
+            if file.endswith(".json"):
+                break
+            else:
+                defs.append(file)
+
+#read json file for corresponding task
+def load_task_vars(name):
+    with open(f"./userdeffs/{name}.json", "r", encoding="utf8") as file:
+            return file.read()
+
 
 
 @app.post("/send")
@@ -47,18 +60,24 @@ async def read_root(payload: DownloadRequest):
 @app.post("/create")
 async def create(payload: create_vars):
     if payload.bash:
-        with open(f"userdeffs/{payload.name}.txt", "w", encoding="utf8") as file:
+        with open(f"userdeffs/{payload.name}", "w", encoding="utf8") as file:
             file.write(f"os.system('{payload.code}')") #user inputted bash script
-            
-            load_defs()             #reload user created functions
-            return {"defs": defs}
+        with open (f"userdeffs/{payload.name}.json", "w", encoding="utf8") as file:
+            file.write(json.dumps(payload.vars))
+            load_defs()             #reload user created tasks
+            return {"vars": payload.vars}
 
 #initial load of tasks upon app launch
-@app.get("/load")
+@app.get("/")
 async def load():
     load_defs()
     return {"defs": defs }
 
 #run user created task next on the list
+@app.post("/prepare")
+async def prepare(payload:retrieve_vars):
+    y = json.loads(load_task_vars(payload.name))
+    return {"name": payload.name, "vars_needed" : y}
+    
 
 
