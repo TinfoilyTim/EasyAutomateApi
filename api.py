01762233f875fef99193a7d867a85fb290ff7330
tim <tim.erlandsson01@gmail.com>
2026-08-13 00:29:35 +0200

    created new function for repeated file reading

--- api.py
+++ api.py
@@ -1,19 +1,15 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from pydantic import BaseModel, JsonValue
 import os
 import json
 
-defs = []
-
 app = FastAPI()
 
 
-#define json payloads for post requests
-class DownloadRequest(BaseModel):
-    path:str
-    url:str
-    zip: bool
-    format: str
+defs = []
+is_json = ".json"
+is_text = ""
+
 
 class create_vars(BaseModel):
     name:str
@@ -29,6 +25,13 @@ class send_vars(BaseModel):
     name:str
     vars:JsonValue
 
+
+
+
+
+
+
+
 #load user created tasks into defs list by reading file names
 def load_defs():
     global defs
@@ -41,9 +44,18 @@ def load_defs():
                 defs.append(file)
 
 #read json file for corresponding task
-def load_task_vars(name):
-    with open(f"./userdeffs/{name}.json", "r", encoding="utf8") as file:
-            return file.read()
+def dyn_read(name, tempj):
+    with open(f"./userdeffs/{name}{tempj}", "r", encoding="utf8") as file:
+        return file.read()
+
+def dyn_write(name, tempj, indata):
+    with open(f"./userdeffs/{name}{tempj}", "w", encoding="utf8") as file:
+        file.write(f"os.system('{indata}')")
+    
+
+
+
+
 
 
     
@@ -58,27 +70,37 @@ async def create(payload: create_vars):
             load_defs()             #reload user created tasks
             return {"vars": payload.vars}
 
+
+
+
 #initial load of tasks upon app launch
 @app.get("/")
 async def load():
     load_defs()
     return {"defs": defs }
 
+
+
+
 #run user created task next on the list
 @app.post("/prepare")
 async def prepare(payload:retrieve_vars):
-    y = json.loads(load_task_vars(payload.name))
+    y = json.loads(load_task_vars(payload.name, is_json))
     return {"name": payload.name, "vars_needed" : y}
     
 
+
+
 #executes the code
 @app.post("/prepare/run")
-async def runtest(payload:send_vars):
-    with open(f"userdeffs/{payload.name}","r", encoding="utf8") as file:
-        data = file.read()
-        #iterates through json variables/item names and replaces every match with its corresponding value from client payload
-        for var,value in payload.vars.items():
-            if f"__{var}__" in data:
-                data = data.replace(f"__{var}__", value)
-        exec(data)
+async def run(payload:send_vars):
+    data = dyn_read(payload.name,is_text)
 
+    #iterates through json variables/item names and replaces every match with its corresponding value from client payload
+    for var,value in payload.vars.items():
+        if f"__{var}__" in data:
+            data = data.replace(f"__{var}__", value)
+    exec(data)
+
+#@app.post("/edit")
+#async def edit
